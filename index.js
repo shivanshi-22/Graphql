b7461a3af502aa7f4e09f847cd0c6885d80be9d7
shivanshi-22 <142731870+shivanshi-22@users.noreply.github.com>
2025-08-12 21:42:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const express = require ("express");
-const {ApolloServer}= require("@apollo/server");
-const {expressMiddleware}= require("@apollo/server/express4");
-const bodyParser= require("body-parser");
-const cors = require("cors");
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-async function startServer(){
-    const app = express;
-    const server = new ApolloServer({});
-    app.use(bodyParser.json());
-    app.use(cors());
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+            type ToDo {
+                id: ID!
+                title: String!
+                completed: Boolean
+            }
+            type Query {
+                getToDo: [ToDo]
+            }
+        `,
+        resolvers: {},
+    });
 
     await server.start();
+    app.use(bodyParser.json());
+    app.use(cors());
     app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8000,()=> console.log("Server started at port 8000."))
+    app.listen(8000, () => console.log("Server started at port 8000."));
 }
+
 startServer();
